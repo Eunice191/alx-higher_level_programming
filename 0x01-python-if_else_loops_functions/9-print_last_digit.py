f2ab8456882527175e6eaadcb2d3f88148e9be8d
Eunice191 <eunicemensah191@gmail.com>
2023-11-28 11:37:23 -0800

    task 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,10 @@
-#!/usr/bin/env python3
-print_last_digit = __import__('9-print_last_digit').print_last_digit
-
-print_last_digit(98)
-print_last_digit(0)
-r = print_last_digit(-1024)
-print(r)
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number < 0:
+        last_digit = abs(number) % 10
+        print(f"{last_digit}", end="")
+        return last_digit
+    else:
+        last_digit = number % 10
+        print(f"{last_digit}", end="")
+        return last_digit
