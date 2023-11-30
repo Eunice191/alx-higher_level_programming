f3ccb3455750221531e7fe65d76e53bf7ad7fee8
Eunice191 <eunicemensah191@gmail.com>
2023-11-30 13:20:31 -0800

    task 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    from add_0 import add
+    from calculator_1 import add, sub, mul, div
 
     a = 10
     b = 5
 
-    # Calculate the result using the imported function
-result = add(a, b)
-
-# Print the result using string formatting
-print("{:d} + {:d} = {:d}".format(a, b, result))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
