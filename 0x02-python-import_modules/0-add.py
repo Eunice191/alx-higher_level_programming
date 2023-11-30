78b9b231790ee1e5a864b63953fee180fb47ed13
Eunice191 <eunicemensah191@gmail.com>
2023-11-30 13:05:42 -0800

    task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
+
+if __name__ == "__main__":
+    """  sum of 1 and 2."""
     from add_0 import add
+
     a = 1
     b = 2
-# Calculate the result using the imported function
-result = add(a, b)
-
-# Print the result using string formatting
-print("{:d} + {:d} = {:d}".format(a, b, result)
+    print("{} + {} = {}".format(a, b, add(a, b)))
