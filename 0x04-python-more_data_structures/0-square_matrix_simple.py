22d67f8940b29d85b09659936860e18369e9d6bc
Eunice191 <eunicemensah191@gmail.com>
2023-12-12 10:00:20 -0800

    task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,15 +1,14 @@
-i#!/usr/bin/python3
+#!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
     new_matrix = []
-    for lists in matrix:
-        new_list = []
-        for x in lists:
-            new_list.append(x ** 2)
-        new_matrix.append(new_list)
+
+    for row in matrix:
+        new_row = []
+
+        for element in row:
+            new_row.append(element ** 2)
+
+        new_matrix.append(new_row)
 
     return new_matrix
-
-# the lambda and map function would look something like this:
-# def square_matrix_simple(matrix=[]):
-# return [list(map(lambda n: n**2, sublist)) for sublist in matrix]
